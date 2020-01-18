2d50a0607499b43c2cea549b1199e00113e8db10
liuuky <liuuky@outlook.com>
2020-01-18 17:31:50 +0800

    change

---
+++ README.md
@@ -0,0 +1,17 @@
+## 介绍
+这是一个Vuepress 搭建的博客文档
+
+## 如何使用
+```
+1. 首先拷贝该项目
+git clone https://github.com/liuuky/vueblog.git
+
+2. 安装依赖
+npm install
+
+3. 本地运行查看（在运行前请进行相关配置）
+npm run docs:dev
+
+4. 推送到 github 的 gh 分支
+npm run deploy
+```
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,11 @@
+const pluginConf = require('../../config/pluginConf')
+const navConf = require('../../config/navConf')
+const sidebarConf = require('../../config/sidebarConf')
+
 module.exports = {
     title: '技术路漫漫且修远，吾将上下而求索.',
     //description 为了更好的SEO
     description: 'Nayek的博客，Vuepress文档',
-    head: [ // 注入到当前页面的 HTML <head> 中的标签
-        ['link', { rel: 'icon', href: '/logo.png' }], // 增加一个自定义的 favicon(网页标签的图标)
-    ],
-    // dest: './dist',
-    base: '/', // 这是部署到github相关的配置
-    markdown: {
-        lineNumbers: false // 代码块显示行号
-    },
     locales: {
         // 键名是该语言所属的子路径
         // 作为特例，默认语言可以使用 '/' 作为其路径。
@@ -17,54 +13,31 @@ module.exports = {
           lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
         },
       },
+    // dest: './dist',
+    base: '/', // 这是部署到github相关的配置
+    markdown: {
+        lineNumbers: false // 代码块显示行号
+    },
+    head: [ // 注入到当前页面的 HTML <head> 中的标签
+        ['link', { rel: 'icon', href: '/logo.png' }],   // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', { rel: 'manifest', href: '/manifest.json' }],  
+    ],
+    // 插件配置
+    plugins: pluginConf,
     themeConfig: {
-        sidebarDepth: 2,
+        sidebarDepth: 2,    // 侧边栏显示2级
         lastUpdated: '上次更新时间', // string | boolean
+        // repo 连接此项目github仓库
         repo: 'liuuky/vueblog',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
         editLinkText: '编辑文档！',
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: '前端·修炼', link: '/web/'},
-            { text: '学习·笔记', link: '/note/'},
-            { text: '博客园', link: 'https://www.cnblogs.com/nayek/' },
-            // { text: 'GitHub', link: 'https://github.com/liuuky' }
-          ],
-        sidebar: {
-            '/web/': [
-                {
-                    title: 'HTML',
-                    collapsable: true,
-                    children: [
-                        '/web/html/行内元素'
-                    ]
-                },
-                {
-                    title: 'CSS',
-                    collapsable: true,
-                    children: [
-                        '/web/css/清除浮动'
-                    ]
-                },
-                {
-                    title: 'JavaScript',
-                    collapsable: true,
-                    children: [
-                        '/web/javascript/',
-                        '/web/javascript/闭包的概念',
-                        '/web/javascript/测试'
-                    ]
-                }
-            ],
-            '/note/': [
-                {
-                    title: 'note'
-                }
-            ]
-        },
-        // sidebarDepth: 2,
-        // lastUpdated: 'Last Updated', // string | boolean
-    
-            // sidebar: 'auto', // 侧边栏配置
-            // sidebarDepth: 2, // 侧边栏显示2级
-    }
+        // 导航栏配置
+        nav: navConf,
+        // 侧边栏配置
+        sidebar: sidebarConf,
+    },
 };
