2c03f089014e041631c7b5a5aaf9c38186b5cb19
liuuky <liuuky@outlook.com>
2020-01-18 12:00:17 +0800

    firstChange

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,13 @@ module.exports = {
     markdown: {
         lineNumbers: false // 代码块显示行号
     },
+    locales: {
+        // 键名是该语言所属的子路径
+        // 作为特例，默认语言可以使用 '/' 作为其路径。
+        '/': {
+          lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+        },
+      },
     themeConfig: {
         sidebarDepth: 2,
         lastUpdated: '上次更新时间', // string | boolean
